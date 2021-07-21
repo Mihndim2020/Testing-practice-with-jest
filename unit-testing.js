82f7e48c29c79b0d56bd9e96f712767cd9e45914
Mihndim Julius <mihndim2016@gmail.com>
2021-07-21 06:03:02 -0400

    Add stringLength function and tests

--- unit-testing.js
+++ unit-testing.js
@@ -1,5 +1,13 @@
-const sum = function(a,b){
-  return a + b;
+// const sum = function(a,b){
+//   return a + b;
+// }
+
+const stringLength = function(string){
+  if(!(string.length > 0 && string.length <= 10)){
+    return "There is an error"
+  }
+  // return string.length;
 }
 
-module.exports = sum;
+// module.exports = sum;
+module.exports = stringLength;
--- unit-testing.test.js
+++ unit-testing.test.js
@@ -1,6 +1,13 @@
-const sum = require('./unit-testing');
+// const sum = require('./unit-testing');
+// import { sum, stringLength } from './unit-testing';
 
-it('works', () => {
-  expect(sum(2, 7)).toEqual(9)
+// it('works', () => {
+//   expect(sum(2, 7)).toEqual(9)
 
+// })
+const stringLength = require('./unit-testing');
+
+it ('Return string lenth', () => {
+  // expect(stringLength('Julius')).toBe(6);
+  expect(stringLength('JuliusNdimMih')).toBe('There is an error')
 })
